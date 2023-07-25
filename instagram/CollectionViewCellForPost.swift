697a121a2bc9f1e24feaab0fe1b56f5da14089f6
ikrrish <kishangovindiya1@gmail.com>
2023-07-25 16:58:03 +0530

    25julykishan

---
+++ instagram/CollectionViewCellForPost.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellForPost.swift
+//  instagram
+//
+//  Created by R82 on 25/07/23.
+//
+
+import UIKit
+
+class CollectionViewCellForPost: UICollectionViewCell {
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+}
--- instagram/mainView.swift
+++ instagram/mainView.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class mainView: UIViewController {
-
+class mainView: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+   
+    var array = ["a"]
+    
+    @IBOutlet weak var cvForPost: UICollectionView!
+    @IBOutlet weak var cvForStory: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
-
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+       
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == cvForStory{
+            let cell = cvForStory.dequeueReusableCell(withReuseIdentifier: "cellForStory", for: indexPath) as! CollectionViewCellForStory
+            cell.storyImage.layer.cornerRadius = 40
+            return cell
+        }
+        let cells = cvForPost.dequeueReusableCell(withReuseIdentifier: "cellForPost", for: indexPath) as! CollectionViewCellForPost
+        cells.userImage.layer.cornerRadius = 25
+        
+        return cells
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == cvForStory{
+            return CGSize(width: 150, height: 150)
+        }
+        return CGSize(width: 392, height: 532)
+    }
+    
 }
 
