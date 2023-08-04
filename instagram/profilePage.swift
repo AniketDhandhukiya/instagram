caa56cade0d3066d73e891914a5eafb3b8259527
ikrrish <kishangovindiya1@gmail.com>
2023-08-04 15:42:51 +0530

    4aug

--- instagram/profilePage.swift
+++ instagram/profilePage.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class profilePage: UIViewController {
 
+    @IBOutlet weak var segmentController: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func controller(){
+        if segmentController.selectedSegmentIndex == 0{
+            
+            
+        }
     }
-    */
-
 }
