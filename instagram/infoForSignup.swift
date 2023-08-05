dc257dbfaa08998886d5b903537168b0a7a317b4
ikrrish <kishangovindiya1@gmail.com>
2023-08-05 15:22:06 +0530

    k

---
+++ instagram/infoForSignup.swift
@@ -0,0 +1,29 @@
+//
+//  infoForSignup.swift
+//  instagram
+//
+//  Created by R82 on 05/08/23.
+//
+
+import UIKit
+
+class infoForSignup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ instagram/previousLogin.swift
@@ -0,0 +1,29 @@
+//
+//  previousLogin.swift
+//  instagram
+//
+//  Created by R82 on 05/08/23.
+//
+
+import UIKit
+
+class previousLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
